6b649ec2bc81ab5febc02b55c6a6e6d2b892a7b8
arpharutyunyan <64978874+arpharutyunyan@users.noreply.github.com>
2021-08-11 20:24:53 +0400

    Lesson17

--- Source/GeometrySandbox/GeometrySandboxGameModeBase.cpp
+++ Source/GeometrySandbox/GeometrySandboxGameModeBase.cpp
@@ -1,5 +1,11 @@
+
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 
 #include "GeometrySandboxGameModeBase.h"
+#include "SandboxPawn.h"
+
+AGeometrySandboxGameModeBase::AGeometrySandboxGameModeBase() {
+	DefaultPawnClass = ASandboxPawn::StaticClass();
+}
 
